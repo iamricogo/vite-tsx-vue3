2b110d6f4b782af7a9088f1d6a3dfd66997430d8
rico <rico@cg.app>
2021-07-31 17:49:20 +0800

    test提交拦截

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,13 +2,13 @@
  * @Author: Rico
  * @Date: 2021-06-12 10:11:29
  * @LastEditors: Rico
- * @LastEditTime: 2021-07-31 17:16:41
+ * @LastEditTime: 2021-07-31 17:48:52
  * @Description:
  */
 const micromatch = require('micromatch')
 const eslintrc = require('./.eslintrc.js')
 module.exports = {
-  'src/**/*.{vue,js,jsx}': allFiles => {
+  'src/**/*.{vue,js,jsx,ts,tsx}': allFiles => {
     //除开eslintrc配置中的排除项目
     const needEsLint = micromatch.not(
       allFiles,
