9de9e23238742824ed512e35a94d745352916f23
rico <rico@cg.app>
2021-07-31 18:49:11 +0800

    chore: 流程

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@
  * @Author: Rico
  * @Date: 2021-07-31 17:28:10
  * @LastEditors: Rico
- * @LastEditTime: 2021-07-31 17:28:26
+ * @LastEditTime: 2021-07-31 18:43:02
  * @Description:
  */
 module.exports = {
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: Rico
  * @Date: 2021-07-31 11:32:44
  * @LastEditors: Rico
- * @LastEditTime: 2021-07-31 18:26:47
+ * @LastEditTime: 2021-07-31 18:46:46
  * @Description:
  */
 import { defineComponent } from 'vue'
@@ -11,6 +11,10 @@ import logo from './assets/logo.png'
 export default defineComponent({
   name: 'App',
   setup: () => {
-    return () => <div>{/* <img alt="Vue logo" src={logo} /> */}</div>
+    return () => (
+      <div>
+        <img alt="Vue logo" src={logo} />
+      </div>
+    )
   }
 })
